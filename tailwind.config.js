1c965ed191f128aa0b636a7ea7f4b9ecff948280
Felix Dietze <github@felx.me>
2024-07-27 18:11:40 +0200

    setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./frontend/src/**/*.scala",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
