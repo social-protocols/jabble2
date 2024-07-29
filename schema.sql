be137486959c9fe1267e9c955a25ab92a7b55178
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-07-29 16:42:32 +0200

    add lineage table and associated trigger and indices

--- schema.sql
+++ schema.sql
@@ -6,10 +6,39 @@ create table user_profile(
 create table post(
   id integer primary key autoincrement -- rowid
   , parent_id integer
-  , author_id text not null references user_profile(user_id)
+  , author_id text not null
   , content text not null
-  , createdAt integer not null default (unixepoch('subsec')*1000)
+  , created_at integer not null default (unixepoch('subsec')*1000)
+  , deleted_at integer default null
+  , is_private integer not null default false
+  , constraint post_parent_id_fkey foreign key (parent_id) references post(id) on delete no action on update no action
+  , constraint post_author_id_fkey foreign key (author_id) references user_profile(user_id) on delete no action on update no action
 ) strict;
+
+create table lineage(
+  ancestor_id integer
+  , descendant_id integer not null
+  , separation integer not null
+  , primary key(ancestor_id, descendant_id)
+) strict;
+create index lineage_ancestor_id on lineage(ancestor_id);
+create index lineage_descendant_id on lineage(descendant_id);
+
+create trigger after_insert_post after insert on post when new.parent_id is not null
+begin
+    -- Insert a lineage record for parent
+    insert into lineage(ancestor_id, descendant_id, separation)
+    values(new.parent_id, new.id, 1) on conflict do nothing;
+
+    -- Insert a lineage record for all ancestors of this parent
+    insert into lineage
+    select
+        ancestor_id
+        , new.id as descendant_id
+        , 1 + separation as separation
+    from lineage ancestor
+    where ancestor.descendant_id = new.parent_id;
+end;
 
 create table vote_event(
   vote_event_id integer not null primary key autoincrement
