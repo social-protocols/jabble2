62107e06c6724b4c0cf4ea5f398965c946212783
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-07-29 21:21:40 +0200

    restructure frontend

--- frontend/src/frontend/FrontendMain.scala
+++ frontend/src/frontend/FrontendMain.scala
@@ -7,14 +7,11 @@ import outwatch.dsl.*
 import colibri.*
 import colibri.reactive.*
 import authn.frontend.*
-import scala.scalajs.js
 import cats.effect.unsafe.implicits.global
-import authn.frontend.authnJS.keratinAuthn.distTypesMod.Credentials
 import webcodegen.shoelace.SlButton.{value as _, *}
 import webcodegen.shoelace.SlButton
-import webcodegen.shoelace.SlInput.{value as _, *}
-import webcodegen.shoelace.SlInput
 import colibri.router.*
+import frontend.components.*
 
 // Outwatch documentation: https://outwatch.github.io/docs/readme.html
 
@@ -94,7 +91,7 @@ def frontPage(refreshTrigger: VarEvent[Unit]) = {
     div(
       createPostForm(refreshTrigger)
     ),
-    postFeed(refreshTrigger),
+    frontPageFeed(refreshTrigger),
     display := "flex",
     flexDirection := "column",
     width := "600px",
@@ -103,7 +100,7 @@ def frontPage(refreshTrigger: VarEvent[Unit]) = {
 }
 
 def loginPage = {
-  authControl(width := "600px", margin := "0 auto")
+  authControl(authnClient)
 }
 
 def postPage(postId: Long, refreshTrigger: VarEvent[Unit]) = {
@@ -112,197 +109,5 @@ def postPage(postId: Long, refreshTrigger: VarEvent[Unit]) = {
     replyTree.map(_.map { tree => postWithReplies(tree, refreshTrigger) }),
     maxWidth := "960px",
     margin := "0 auto",
-  )
-}
-
-def postWithReplies(replyTree: rpc.ReplyTree, refreshTrigger: VarEvent[Unit]): VNode = {
-  div(
-    postDetails(replyTree.post, refreshTrigger),
-    replyTree.replies.map { tree => postWithReplies(tree, refreshTrigger) },
-    marginLeft := "50px",
-  )
-}
-
-def postDetails(post: rpc.Post, refreshTrigger: VarEvent[Unit]): VNode = {
-  div(
-    postInfoBar(post),
-    post.content,
-    postActionBar(post, refreshTrigger),
-  )
-}
-
-def postInfoBar(post: rpc.Post): VNode = {
-  div("created at: ", post.createdAt)
-}
-
-def postActionBar(post: rpc.Post, refreshTrigger: VarEvent[Unit]): VNode = {
-  val contentState = Var("")
-
-  val showReplyForm = Var(false)
-
-  div(
-    div(
-      button(
-        "⇧",
-        onClick.doEffect {
-          RpcClient.call.vote(postId = post.id, parentId = post.parentId, direction = rpc.Direction.Up)
-        },
-      ),
-      span("Vote"),
-      button(
-        "⇩",
-        onClick.doEffect {
-          RpcClient.call.vote(postId = post.id, parentId = post.parentId, direction = rpc.Direction.Down)
-        },
-      ),
-      button(
-        "🗨 Reply",
-        onClick.doAction {
-          showReplyForm.update(!_)
-        },
-      ),
-      showReplyForm.map { show =>
-        if (show) {
-          button(
-            "✕",
-            cls := "ml-auto self-center pr-2",
-            onClick.doAction {
-              showReplyForm.set(false)
-            },
-          )
-        } else {
-          VMod.empty
-        }
-      },
-      cls := "flex w-full flex-wrap items-start gap-3 text-xl opacity-50 sm:text-base",
-    ),
-    showReplyForm.map { show =>
-      if (show) {
-        div(
-          slInput(
-            SlInput.placeholder := "Enter your reply",
-            value <-- contentState,
-            onSlInput.value --> contentState,
-          ),
-          slButton(
-            "Reply",
-            onClick(contentState).foreachEffect { content =>
-              lift {
-                unlift(RpcClient.call.createReply(parentId = post.id, content = content))
-                refreshTrigger.set(())
-              }
-            },
-          ),
-        )
-      } else {
-        VMod.empty
-      }
-    },
-  )
-}
-
-def authControl = {
-
-  val usernameState = Var("")
-  val passwordState = Var("")
-
-  div(
-    slInput(
-      SlInput.placeholder := "Username",
-      value <-- usernameState,
-      onSlInput.map(_.target.value) --> usernameState,
-    ),
-    slInput(
-      SlInput.placeholder := "Password",
-      SlInput.`type` := "password",
-      value <-- passwordState,
-      onSlInput.map(_.target.value) --> passwordState,
-    ),
-    slButton(
-      "Register",
-      onClick(usernameState).withLatest(passwordState).foreachEffect { case (username, password) =>
-        RpcClient.call.register(username = username, password = password)
-      },
-    ),
-    slButton(
-      "Login",
-      onClick(usernameState).withLatest(passwordState).foreachEffect { case (username, password) =>
-        authnClient.login(Credentials(username = username, password = password))
-      },
-    ),
-    // b(authn.session),
-    slButton(
-      "Logout",
-      onClick.doEffect {
-        authnClient.logout
-      },
-    ),
-  )
-}
-
-def createPostForm(refreshTrigger: VarEvent[Unit]) = {
-  val contentState = Var("")
-
-  div(
-    slInput(
-      SlInput.placeholder := "What's on your mind?",
-      value <-- contentState,
-      onSlInput.map(_.target.value) --> contentState,
-    ),
-    slButton(
-      "Post",
-      onClick(contentState).foreachEffect { content =>
-        lift {
-          unlift(RpcClient.call.createPost(content = content))
-          refreshTrigger.set(())
-        }
-      },
-    ),
-    display := "flex",
-    width := "600px",
-    border := "1px solid lightgrey",
-    padding := "10px",
-  )
-}
-
-def postFeed(refreshTrigger: VarEvent[Unit]) = {
-  div(
-    lift {
-      div(
-        unlift(RpcClient.call.getPosts()).map(post => postCard(post.id, post.content, post.authorId, refreshTrigger)),
-        display := "flex",
-        flexDirection := "column",
-      )
-    }
-  )
-}
-
-def postCard(postId: Long, content: String, authorId: String, refreshTrigger: VarEvent[Unit]) = {
-  val contentState = Var("")
-
-  div(
-    div("by: ", authorId, color := "grey"),
-    content,
-    div(
-      slInput(
-        SlInput.placeholder := "Reply",
-        value <-- contentState,
-        onSlInput.value --> contentState,
-      ),
-      slButton(
-        "Reply",
-        onClick(contentState).foreachEffect { content =>
-          lift {
-            unlift(RpcClient.call.createReply(parentId = postId, content = content))
-            refreshTrigger.set(())
-          }
-        },
-      ),
-      display := "flex",
-      width := "100%",
-    ),
-    width := "600px",
-    border := "1px solid lightgrey",
-    padding := "10px",
   )
 }
