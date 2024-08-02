c6a3e0d57d539b2785f31b68f35b697876abe15d
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-08-02 13:51:24 +0200

    tweak login and signup page

--- frontend/src/frontend/components/AuthControl.scala
+++ frontend/src/frontend/components/AuthControl.scala
@@ -13,32 +13,55 @@ import webcodegen.shoelace.SlInput.{value as _, *}
 import webcodegen.shoelace.SlInput
 
 def authControl(authnClient: AuthnClient[IO]) = {
-  val usernameState = Var("")
-  val passwordState = Var("")
+  val usernameForLoginState = Var("")
+  val passwordForLoginState = Var("")
+
+  val usernameForSignupState = Var("")
+  val passwordForSignupState = Var("")
 
   div(
-    slInput(
-      SlInput.placeholder := "Username",
-      value <-- usernameState,
-      onSlInput.map(_.target.value) --> usernameState,
+    cls := "flex flex-col gap-16",
+    div(
+      cls := "flex flex-col gap-2",
+      h2("Login", cls := "text-xl font-bold text-center"),
+      slInput(
+        SlInput.placeholder := "Username",
+        value <-- usernameForLoginState,
+        onSlInput.map(_.target.value) --> usernameForLoginState,
+      ),
+      slInput(
+        SlInput.placeholder := "Password",
+        SlInput.`type` := "password",
+        value <-- passwordForLoginState,
+        onSlInput.map(_.target.value) --> passwordForLoginState,
+      ),
+      slButton(
+        "Login",
+        onClick(usernameForLoginState).withLatest(passwordForLoginState).foreachEffect { case (username, password) =>
+          authnClient.login(Credentials(username = username, password = password))
+        },
+      ),
     ),
-    slInput(
-      SlInput.placeholder := "Password",
-      SlInput.`type` := "password",
-      value <-- passwordState,
-      onSlInput.map(_.target.value) --> passwordState,
-    ),
-    slButton(
-      "Register",
-      onClick(usernameState).withLatest(passwordState).foreachEffect { case (username, password) =>
-        RpcClient.call.register(username = username, password = password)
-      },
-    ),
-    slButton(
-      "Login",
-      onClick(usernameState).withLatest(passwordState).foreachEffect { case (username, password) =>
-        authnClient.login(Credentials(username = username, password = password))
-      },
+    div(
+      cls := "flex flex-col gap-2",
+      h2("Create account", cls := "text-xl font-bold text-center"),
+      slInput(
+        SlInput.placeholder := "Username",
+        value <-- usernameForSignupState,
+        onSlInput.map(_.target.value) --> usernameForSignupState,
+      ),
+      slInput(
+        SlInput.placeholder := "Password",
+        SlInput.`type` := "password",
+        value <-- passwordForSignupState,
+        onSlInput.map(_.target.value) --> passwordForSignupState,
+      ),
+      slButton(
+        "Create account",
+        onClick(usernameForSignupState).withLatest(passwordForSignupState).foreachEffect { case (username, password) =>
+          RpcClient.call.register(username = username, password = password)
+        },
+      ),
     ),
     // b(authn.session),
     slButton(
@@ -47,7 +70,5 @@ def authControl(authnClient: AuthnClient[IO]) = {
         authnClient.logout
       },
     ),
-    width := "600px",
-    margin := "0 auto",
   )
 }
