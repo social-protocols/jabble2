ef807251178a91a1f5f7391e1c3605512158db8b
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-08-13 17:37:41 +0200

    implement collapsing feature

--- frontend/src/frontend/components/PostWithReplies.scala
+++ frontend/src/frontend/components/PostWithReplies.scala
@@ -13,22 +13,40 @@ import cats.effect.IO
 import webcodegen.shoelace.SlIcon.*
 import webcodegen.shoelace.SlIcon
 
-def postWithReplies(postTree: rpc.PostTree, treeContext: TreeContext, refreshTrigger: VarEvent[Unit]): VNode = {
+def postWithReplies(postTree: rpc.PostTree, treeContext: TreeContext, refreshTrigger: VarEvent[Unit]): VMod = {
+  val hidePost = treeContext.collapsedState.hidePost.getOrElse(postTree.post.id, false)
+  val hideChildren = treeContext.collapsedState.hideChildren.getOrElse(postTree.post.id, false)
+
   val postData = treeContext.postTreeDataState.posts(postTree.post.id)
-  div(
-    postDetails(postTree.post, postData, treeContext, refreshTrigger),
+
+  if (!hidePost) {
     div(
-      postTree.replies.map { tree => postWithReplies(tree, treeContext, refreshTrigger) },
-      cls := "ml-2 pl-3",
-    ),
-  )
+      postDetails(postTree.post, postData, postTree, treeContext, refreshTrigger),
+      if (!hideChildren) {
+        div(
+          postTree.replies.map { tree => postWithReplies(tree, treeContext, refreshTrigger) },
+          cls := "ml-2 pl-3",
+        )
+      } else {
+        VMod.empty
+      }
+    )
+  } else {
+    VMod.empty
+  }
 }
 
-def postDetails(post: rpc.Post, postData: rpc.PostData, treeContext: TreeContext, refreshTrigger: VarEvent[Unit]): VNode = {
+def postDetails(
+  post: rpc.Post,
+  postData: rpc.PostData,
+  postTree: rpc.PostTree,
+  treeContext: TreeContext,
+  refreshTrigger: VarEvent[Unit],
+): VNode = {
   div(
     postInfoBar(post, postData),
     post.content,
-    postActionBar(post, treeContext, refreshTrigger),
+    postActionBar(post, postTree, treeContext, refreshTrigger),
     cls := "mb-4",
   )
 }
@@ -73,7 +91,7 @@ def postInfoBar(post: rpc.Post, postData: rpc.PostData): VNode = {
   )
 }
 
-def postActionBar(post: rpc.Post, treeContext: TreeContext, refreshTrigger: VarEvent[Unit]): VNode = {
+def postActionBar(post: rpc.Post, postTree: rpc.PostTree, treeContext: TreeContext, refreshTrigger: VarEvent[Unit]): VNode = {
   val contentState = Var("")
 
   val showReplyForm = Var(false)
@@ -87,9 +105,40 @@ def postActionBar(post: rpc.Post, treeContext: TreeContext, refreshTrigger: VarE
   val upvoteIcon   = if (currentVote == rpc.Direction.Up) "arrow-up-circle-fill" else "arrow-up-circle"
   val downvoteIcon = if (currentVote == rpc.Direction.Down) "arrow-down-circle-fill" else "arrow-down-circle"
 
+  val hasChildren    = postTree.replies.nonEmpty
+  val childrenHidden = treeContext.collapsedState.hideChildren.getOrElse(post.id, false)
+
+  def toggleHideChildren() = {
+    if (childrenHidden) {
+      // expand
+      var newHideChildrenState = treeContext.collapsedState.hideChildren.updated(post.id, false)
+      // collapse direct children
+      postTree.replies.foreach { reply =>
+        {
+          newHideChildrenState = newHideChildrenState.updated(reply.post.id, true)
+        }
+      }
+      treeContext.setCollapsedState(treeContext.collapsedState.copy(hideChildren = newHideChildrenState))
+    } else {
+      // collapse
+      treeContext.setCollapsedState(
+        treeContext.collapsedState.copy(hideChildren = treeContext.collapsedState.hideChildren.updated(post.id, true))
+      )
+    }
+  }
+
   div(
     div(
       cls := "flex w-full flex-wrap items-start gap-3 text-xl opacity-50 sm:text-base",
+      if (hasChildren) {
+        if (childrenHidden) {
+          button(slIcon(SlIcon.name := "chevron-right"), title := "Expand this comment", onClick.doAction { toggleHideChildren() })
+        } else {
+          button(slIcon(SlIcon.name := "chevron-down"), title := "Collapse this comment", onClick.doAction { toggleHideChildren() })
+        }
+      } else {
+        VMod.empty
+      },
       button(
         slIcon(SlIcon.name := upvoteIcon),
         onClick.doEffect { submitVote(rpc.Direction.Up) },
