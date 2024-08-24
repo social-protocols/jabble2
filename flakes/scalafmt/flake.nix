ad7c9365e9106ed924afc26a75bd033451c9e4fa
Felix Dietze <github@felx.me>
2024-08-24 22:22:11 +0200

    make scalafmt flake work on macos

--- flakes/scalafmt/flake.nix
+++ flakes/scalafmt/flake.nix
@@ -4,31 +4,46 @@
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs"; };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      version = builtins.readFile ./.scalafmt.conf;
-      scalafmtVersion = builtins.match ''.*version = "([^"]*)".*'' version;
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in {
-      scalafmt = pkgs.stdenv.mkDerivation {
-        pname = "scalafmt";
-        version = builtins.head scalafmtVersion;
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          version = builtins.readFile ./.scalafmt.conf;
+          scalafmtVersion =
+            builtins.head (builtins.match ''.*version = "([^"]*)".*'' version);
 
-        src = pkgs.fetchurl {
-          url = "https://github.com/scalameta/scalafmt/releases/download/v${
-              builtins.head scalafmtVersion
-            }/scalafmt-linux-musl";
-          sha256 = "sha256-tZbltEBiFJz2303dgYAkCFH0f43EQSy9m4abwz3z7bs=";
-        };
+          urlForSystem = if pkgs.stdenv.isDarwin then
+            "https://github.com/scalameta/scalafmt/releases/download/v${scalafmtVersion}/scalafmt-macos"
+          else
+            "https://github.com/scalameta/scalafmt/releases/download/v${scalafmtVersion}/scalafmt-linux-musl";
 
-        phases = [ "installPhase" ];
+          sha256ForSystem = if pkgs.stdenv.isDarwin then
+            "sha256-0rA1IPbiqEGxc6/vuW2ab3BnJskjZxviPCgUPyQKmLY="
+          else
+            "sha256-tZbltEBiFJz2303dgYAkCFH0f43EQSy9m4abwz3z7bs=";
+        in {
+          scalafmt = pkgs.stdenv.mkDerivation {
+            pname = "scalafmt";
+            version = scalafmtVersion;
 
-        installPhase = ''
-          mkdir -p $out/bin
-          cp $src $out/bin/scalafmt
-          chmod +x $out/bin/scalafmt
-        '';
-      };
+            src = pkgs.fetchurl {
+              url = urlForSystem;
+              sha256 = sha256ForSystem;
+            };
+
+            phases = [ "installPhase" ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $src $out/bin/scalafmt
+              chmod +x $out/bin/scalafmt
+            '';
+
+          };
+        });
     };
-  };
 }
